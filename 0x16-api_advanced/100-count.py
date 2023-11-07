88764725a2550dd81a62f4f849b9ef14b635ac81
AflaxCade <116007622+AflaxCade@users.noreply.github.com>
2023-11-07 14:38:01 +0300

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""Reddit API"""
+""" raddit api"""
 
 import json
 import requests
 
-def count_words(subreddit, word_list, after="", count=None):
-    """Count all words"""
 
-    if count is None:
+def count_words(subreddit, word_list, after="", count=[]):
+    """count all words"""
+
+    if after == "":
         count = [0] * len(word_list)
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     request = requests.get(url,
                            params={'after': after},
                            allow_redirects=False,
@@ -19,7 +20,7 @@ def count_words(subreddit, word_list, after="", count=None):
     if request.status_code == 200:
         data = request.json()
 
-        for topic in data['data']['children']:
+        for topic in (data['data']['children']):
             for word in topic['data']['title'].split():
                 for i in range(len(word_list)):
                     if word_list[i].lower() == word.lower():
@@ -39,15 +40,15 @@ def count_words(subreddit, word_list, after="", count=None):
                     if (count[j] > count[i] or
                             (word_list[i] > word_list[j] and
                              count[j] == count[i])):
-                        aux_count = count[i]
+                        aux = count[i]
                         count[i] = count[j]
-                        count[j] = aux_count
-                        aux_word = word_list[i]
+                        count[j] = aux
+                        aux = word_list[i]
                         word_list[i] = word_list[j]
-                        word_list[j] = aux_word
+                        word_list[j] = aux
 
             for i in range(len(word_list)):
-                if count[i] > 0 and i not in save:
-                    print(f"{word_list[i].lower()}: {count[i]}")
+                if (count[i] > 0) and i not in save:
+                    print("{}: {}".format(word_list[i].lower(), count[i]))
         else:
             count_words(subreddit, word_list, after, count)
